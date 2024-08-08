02a5c2dd75990a19f5c1e146a9ef2f186fd87a0b
wanghuaijin <hjwang2334@163.com>
2024-08-08 23:23:48 +0800

    Test

---
+++ .vuepress/navbar.ts
@@ -0,0 +1,12 @@
+import {NavItem} from "vuepress/config";
+
+export default [
+    {
+        text: "学习路线",
+        link: '/Reading/'
+    },
+    {
+        text: "编程路线",
+        link: '/Reading/'
+    },
+] as NavItem[];
---
+++ .vuepress/sidebars/roadmapSideBar.ts
@@ -0,0 +1,11 @@
+export default [
+  "",
+  {
+    title: "Reading",
+    collapsable: false,
+    children: [
+      "高效能人士的七个习惯.md",
+      "如何阅读一本书.md",
+    ],
+  },
+];
